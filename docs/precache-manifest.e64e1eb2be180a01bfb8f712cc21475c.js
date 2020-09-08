e9e4c30d95e31ffe12a4bf78cf0e9352b6668003
Senthilkannan <51955692+techkanna@users.noreply.github.com>
2020-09-08 13:42:34 +0530

    :rocket: added build files

---
+++ docs/precache-manifest.e64e1eb2be180a01bfb8f712cc21475c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021205d47e587ffa8dda5d2ddf8c1bb5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "38ecda9e83fac7841249",
+    "url": "./static/css/main.d3870d3b.chunk.css"
+  },
+  {
+    "revision": "1131ae48b3ca643fa884",
+    "url": "./static/js/2.1bb9cf4b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.1bb9cf4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38ecda9e83fac7841249",
+    "url": "./static/js/main.a92f685c.chunk.js"
+  },
+  {
+    "revision": "c7f53c401b106a4dcea7",
+    "url": "./static/js/runtime-main.e2b91465.js"
+  }
+]);
